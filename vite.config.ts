757e77d8580a0c7369a4a456bf55534415001e57
mahmoodhussam <39731573+mahmoodhussam@users.noreply.github.com>
2025-06-01 09:17:43 +0300

    change the base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ghofran-plastic-slider-site",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
